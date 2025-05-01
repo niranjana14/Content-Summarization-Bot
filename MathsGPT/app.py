385058303aa228a1ee7de25dfa0b5df25a5b0cbb
niranjana14 <niranju1433@gmail.com>
2025-05-01 22:54:10 +0530

    Updated app.py

--- MathsGPT/app.py
+++ MathsGPT/app.py
@@ -7,15 +7,15 @@ from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
 from langchain.callbacks import StreamlitCallbackHandler
 
-## Set upi the Stramlit app
-st.set_page_config(page_title="Text To MAth Problem Solver And Data Serach Assistant",page_icon="🧮")
+## Set upi the Streamlit app
+st.set_page_config(page_title="Text To MAth Problem Solver And Data Search Assistant",page_icon="🧮")
 st.title("Text To Math Problem Solver Uing Google Gemma 2")
 
 groq_api_key=st.sidebar.text_input(label="Groq API Key",type="password")
 
 
 if not groq_api_key:
-    st.info("Please add your Groq APPI key to continue")
+    st.info("Please add your Groq API key to continue")
     st.stop()
 
 llm=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
@@ -26,11 +26,11 @@ wikipedia_wrapper=WikipediaAPIWrapper()
 wikipedia_tool=Tool(
     name="Wikipedia",
     func=wikipedia_wrapper.run,
-    description="A tool for searching the Internet to find the vatious information on the topics mentioned"
+    description="A tool for searching the Internet to find the various information on the topics mentioned"
 
 )
 
-## Initializa the MAth tool
+## Initialize the MAth tool
 
 math_chain=LLMMathChain.from_llm(llm=llm)
 calculator=Tool(
@@ -40,7 +40,7 @@ calculator=Tool(
 )
 
 prompt="""
-Your a agent tasked for solving users mathemtical question. Logically arrive at the solution and provide a detailed explanation
+Your a agent tasked for solving users mathematical question. Logically arrive at the solution and provide a detailed explanation
 and display it point wise for the question below
 Question:{question}
 Answer:
@@ -79,7 +79,7 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg['content'])
 
 ## LEts start the interaction
-question=st.text_area("Enter youe question:","I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruit do I have at the end?")
+question=st.text_area("Enter your question:","I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruit do I have at the end?")
 
 if st.button("find my answer"):
     if question:
